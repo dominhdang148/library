b61ee2a8fb3f0353869378423dfe3eda216e76e1
dominhdang148 <myclone235@gmail.com>
2022-12-10 08:29:02 +0700

    Build and migrate database ✅

--- base/models.py
+++ base/models.py
@@ -11,7 +11,29 @@ class Topic(models.Model):
 
 
 class Document(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=true)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=true)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = model.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body
